1997bf5df985c0486be8c30028441ec39565ad99
Rohan D'souza <dsouzarohan239@gmail.com>
2018-02-08 04:50:34 +0530

    'linkeddb'

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-    database: "mongodb://localhost:27017/classDB",
+    database: "mongodb://dexter:xavclassroom@ds229448.mlab.com:29448/xaviersclassrooms",
     secret: 'saymyname'
 }
